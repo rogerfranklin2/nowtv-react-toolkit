57c105e67066fb2a543a8c30751d537516f9622c
Noah Ark <nowarkdev@gmail.com>
2015-06-02 12:17:18 +0100

    LDA|RF: Stripping rubbish from the input

--- src/components/Input/Input.react.js
+++ src/components/Input/Input.react.js
@@ -3,31 +3,13 @@ import React from 'react';
 class Input extends React.Component {
 	constructor(props){
 		super(props)
-		this.handleChange = this.handleChange.bind(this);
-
-        this.state = {
-            requiredError: false
-        };
-	}
-
-	handleChange(event) {
-		console.log("HANDLE ", event.target.value.length == 0)
-		if (event.target.value.length == 0){
-    		this.setState({requiredError: true});	
-		}
 	}
 
     render() {
-    	var required;
-    	if (this.state.requiredError){
-    		required = <span>This element is required</span>
-    	}
-
         return (
         	<span>
-        	<label for={this.props.name}>{this.props.name}</label>
-            <input id={this.props.name} name={this.props.name} onChange={this.handleChange} placeholder={this.props.placeholder} className={"testInput"}></input>
-            { required }
+				<label for={this.props.name}>{this.props.name}</label>
+				<input id={this.props.name} name={this.props.name}  className={"testInput"}/>
             </span>
         )
     }
