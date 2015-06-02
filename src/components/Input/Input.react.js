9edfc99c5534049d384d00aab38dd557397d8b42
Noah Ark <nowarkdev@gmail.com>
2015-06-02 17:49:44 +0100

    LDA|RF: Moving import styles into the external sass

--- src/components/Input/Input.react.js
+++ src/components/Input/Input.react.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import styles from './InputStyles.sass'
-
 class Input extends React.Component {
   constructor(props){
     super(props)
